d4dda95f5edfa665c0d6a4fd161d7efb2c9bf1b2
Magdiel <lopmag.lopez@gmail.com>
2018-06-18 08:35:15 -0600

    page to login to the app

--- www/js/sizes.js
+++ www/js/sizes.js
@@ -1,5 +1,9 @@
 window.addEventListener('load',function(){
     const Y = window.innerHeight;
     const X = window.innerWidth;
-    
+    const footer = document.getElementById('foot1');
+    const btnNew = document.getElementById('btnNew');
+    footer.style.top = (Y-440)+'px';
+    btnNew.style.top = (Y-150)+'px';
+    btnNew.style.left = ((X/2)-52)+'px';
 });
